a46b9c1be4393fb3e90539e5b48f21fe5f3ec108
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2015-03-04 16:50:06 +1300

    Can now insert new contacts into postres db

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var _ = require('lodash');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json());
+
+app.post('/contacts', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            console.error('Failed to connect to database: ' + err);
+            res.send({
+                success: false
+            });
+        }
+        var insert = 'INSERT INTO contact(name) ' +
+                        'VALUES($1)';
+        var newContact = req.body;
+        client.query(insert, [ newContact.Name ], function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('Successfully inserted contact');
+                res.send({
+                    success: true
+                });
+            }
+        });
+    });
+});
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
+    console.log('Database URL: ' + process.env.DATABASE_URL);
 });
--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -9,9 +9,15 @@ app.controller('MainCtrl', function($scope, $http) {
 	$scope.isError = false;
 
 	$scope.newContact = {
+		Name: 'Luke'
 	};
 
 	$scope.save = function() {
+		$scope.isSaving = true;
+		$http.post('/contacts', $scope.newContact).then(function(res) {
+			console.log('Received response: ', res);
+			$scope.isSaving = false;
+		});
 		$scope.newContact = {};
 	};
 
