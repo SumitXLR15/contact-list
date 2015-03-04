7cb6406016740368f1ec874deb0ff4e0ac8bf79d
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2015-03-04 17:03:40 +1300

    Convert tabs to spaces.

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -5,20 +5,20 @@
  * @date   March 2015 
  */
 app.controller('MainCtrl', function($scope, $http) {
-	$scope.isSaving = false;
-	$scope.isError = false;
+    $scope.isSaving = false;
+    $scope.isError = false;
 
-	$scope.newContact = {
-		Name: 'Luke'
-	};
+    $scope.newContact = {
+        Name: 'Luke'
+    };
 
-	$scope.save = function() {
-		$scope.isSaving = true;
-		$http.post('/contacts', $scope.newContact).then(function(res) {
-			console.log('Received response: ', res);
-			$scope.isSaving = false;
-		});
-		$scope.newContact = {};
-	};
+    $scope.save = function() {
+        $scope.isSaving = true;
+        $http.post('/contacts', $scope.newContact).then(function(res) {
+            console.log('Received response: ', res);
+            $scope.isSaving = false;
+        });
+        $scope.newContact = {};
+    };
 
 });
